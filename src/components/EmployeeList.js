df7a3cdc0f0c0142c470a04b5de56b3a543237f1
Yasemin Koc <yaseminkocc37@gmail.com>
2022-08-27 04:08:51 +0300

    project uploaded

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,21 @@
+import Employee from "./Employee";
+const EmployeeList = () => {
+    return (
+        <table className="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Address</th>
+                    <th>Phone</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <Employee />
+            </tbody>
+        </table>
+    );
+}
+
+export default EmployeeList;
